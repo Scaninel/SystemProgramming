3d0744e8c31be337ed64acf6345908eb5069f885
Can <inelscan@gmail.com>
2022-12-11 23:08:16 +0300

    copy feature was added to dd

--- dd.c
+++ dd.c
@@ -3,14 +3,17 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #define DEFAULT_BUFFER_SIZE 512
 
 void exit_sys(const char *msg);
 
+int inFd, destFd;
+int blockSize;
+
 int main(int argc, char *argv[])
 {
-    int inFd, destFd;
 
     if (argc < 2)
     {
@@ -20,6 +23,10 @@ int main(int argc, char *argv[])
 
     char *ifPos = strstr(argv[1], "if=");
     char *ofPos = strstr(argv[2], "of=");
+    char *bs = NULL;
+
+    if (argc > 3)
+        bs = strstr(argv[3], "bs=");
 
     if (ifPos == NULL || ofPos == NULL || ((ifPos - argv[1]) != 0) || ((ofPos - argv[2]) != 0))
     {
@@ -29,6 +36,17 @@ int main(int argc, char *argv[])
 
     char *inputFileName = &argv[1][3];
     char *destFileName = &argv[2][3];
+
+    if ((argc < 3) || (bs == NULL) || ((bs - argv[3]) != 0))
+    {
+        printf("block size is defined as 512 bytes as default\n");
+        blockSize = 512;
+    }
+    else
+    {
+        blockSize = atoi(&argv[3][3]);
+        printf("block size is defined by user as '%d-bytes'\n", blockSize);
+    }
 
     // if ((strstr(inputFileName, " ") != NULL) || (strstr(destFileName, " ") != NULL))
     // {
@@ -42,9 +60,25 @@ int main(int argc, char *argv[])
     if ((inFd = open(inputFileName, O_RDONLY)) == -1)
         exit_sys(argv[1]);
 
-    if ((destFd = open(destFileName, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)) == -1)
+    if ((destFd = open(destFileName, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
         exit_sys(argv[2]);
 
+    char *readBuf = malloc(blockSize);
+    if (readBuf == NULL)
+        exit_sys("insufficent memory for read buffer\n");
+
+    int f_readSize = -1;
+
+    do
+    {
+        f_readSize = read(inFd, readBuf, blockSize);
+        if(f_readSize <= 0)
+            break;
+
+        write(destFd,readBuf,f_readSize);
+        memset(readBuf,0,blockSize);
+
+    } while (f_readSize > 0);
 
 
     return 0;
