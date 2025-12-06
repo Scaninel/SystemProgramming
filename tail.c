716f030adc24f395a253e56757ee6f9c2075ac95
S.Can İNEL <107362800+Scaninel@users.noreply.github.com>
2025-12-06 15:07:39 +0300

    Refactor tail.c for improved option handling

--- tail.c
+++ tail.c
@@ -3,29 +3,32 @@
 #include <getopt.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <string.h>
+
+#define DEFAULT_LINE_NUMBER 10
 
 int main(int argc, char *argv[])
 {
     int result, index;
-    int c_flag, n_flag, v_flag, version_flag, help_flag, bytes_flag, lines_flag;
+    int verbose_flag, version_flag, help_flag, bytes_flag, lines_flag;
     char *byte_arg, *line_arg;
 
     int i = 0;
     int errFlag = 0;
     opterr = 0;
 
-    c_flag = n_flag = v_flag = version_flag = help_flag = bytes_flag = lines_flag = 0;
+    verbose_flag = version_flag = help_flag = bytes_flag = lines_flag = 0;
     byte_arg = line_arg = NULL;
 
     char *inputFileName = NULL;
 
     struct option options[] = {
-        {"bytes", required_argument, NULL, 2}, // "help" argümanın adı, "no_argument" seçenekli/seçeneksiz olması,
-        {"lines", required_argument, NULL, 3}, // 3.parametreye NULL geçilirse getopy_long argümanı bulursa 4. parametreye girilen değeri geri döner
-        {"verbose", no_argument, &v_flag, 1},  // 3.parametreye bir adres girilirse ör: &helpFlg, getopt_long eğer argümanı bulursa helpFlg'yi
+        {"bytes", required_argument, NULL, 'c'}, // "help" argümanın adı, "no_argument" seçenekli/seçeneksiz olması,
+        {"lines", required_argument, NULL, 'n'}, // 3.parametreye NULL geçilirse getopy_long argümanı bulursa 4. parametreye girilen değeri geri döner
+        {"verbose", no_argument, &verbose_flag, 'v'},  // 3.parametreye bir adres girilirse ör: &helpFlg, getopt_long eğer argümanı bulursa helpFlg'yi 4. parametredeki değer ile set eder
         {"version", no_argument, &version_flag, 1},
         {"help", no_argument, &help_flag, 1},
-        {0, 0, 0, 0}, // 4. parametredeki değer ile set eder
+        {0, 0, 0, 0},
     };
 
     while ((result = getopt_long(argc, argv, "c:n:v", options, &index)) != -1)
@@ -34,42 +37,30 @@ int main(int argc, char *argv[])
         {
         case 'c':
         {
-            c_flag = 1;
-            byte_arg = optarg;
-            ;
-            break;
-        }
-        case 'n':
-        {
-            n_flag = 1;
-            line_arg = optarg;
-            break;
-        }
-        case 'v':
-        {
-            v_flag = 1;
-            break;
-        }
-
-        case 2:
-        {
             bytes_flag = 1;
             byte_arg = optarg;
             break;
         }
-        case 3:
+        case 'n':
         {
             lines_flag = 1;
             line_arg = optarg;
             break;
         }
+        case 'v':
+        {
+            verbose_flag = 1;
+            break;
+        }
 
-        case '?':
+        case '?': // hata durumunda getopt_long "?" geri döner
         {
             if (optopt == 'c')
-                fprintf(stderr, "-c or --bytes option must have an argument\n");
+                fprintf(stderr, "-c option must have an argument\n");
             else if (optopt == 'n')
-                fprintf(stderr, "-n or --lines option must have an argument\n");
+                fprintf(stderr, "-n option must have an argument\n");
+            else if (optopt != 0)
+                fprintf(stderr, "invalid option: -%c\n", optopt);
             else
                 fprintf(stderr, "invalid long option...\n");
 
@@ -82,98 +73,23 @@ int main(int argc, char *argv[])
     if (errFlag)
         exit(EXIT_FAILURE);
 
-    if ((c_flag && n_flag) || (bytes_flag && lines_flag))
-    {
-        printf("-c option and -n option can not be used at the same time\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (optind != argc)
+    if (optind != argc) // seçeneksiz argüman
     {
         inputFileName = argv[optind];
+        // printf("input file name: %s\n", inputFileName);
     }
     else
     {
-        if (!help_flag)
+        if (!help_flag && !version_flag)
         {
-            printf("NO INPUT FILE NAME\n");
-            exit(EXIT_FAILURE);
+            printf("NO INPUT FILE NAME, stdin is used as input\n");
+            inputFileName = NULL;
+            // exit(EXIT_FAILURE);
         }
     }
 
-    if (v_flag)
+    if (verbose_flag)
         printf("===============================\nfile \"%s\" is written as below\n===============================\n\n", inputFileName);
-
-    if (c_flag || bytes_flag)
-    {
-        FILE *fp = fopen(inputFileName, "r");
-
-        if (!fp)
-        {
-            perror("File opening failed");
-            exit(EXIT_FAILURE);
-        }
-
-        fseek(fp, 0, SEEK_END);
-
-        int readRes = 0;
-        char readByte = 0;
-        int readCount = 0;
-
-        do
-        {
-            fseek(fp, -2, SEEK_CUR);
-            readRes = fread(&readByte, 1, 1, fp);
-            readCount++;
-
-        } while (readRes && ftell(fp) >= 2 && readCount < atoi(byte_arg));
-
-        readByte = 0;
-
-        fseek(fp, -1, SEEK_CUR);
-
-        while (fread(&readByte, 1, 1, fp))
-        {
-            printf("%c", readByte);
-        }
-    }
-
-    if (n_flag || lines_flag)
-    {
-        FILE *fp = fopen(inputFileName, "r");
-
-        if (!fp)
-        {
-            perror("File opening failed");
-            exit(EXIT_FAILURE);
-        }
-
-        fseek(fp, 0, SEEK_END);
-
-        int readRes = 0;
-        char readByte = 0;
-        int readLineCnt = 0;
-
-        do
-        {
-            fseek(fp, -2, SEEK_CUR);
-            readRes = fread(&readByte, 1, 1, fp);
-            if (readByte == '\n')
-                readLineCnt++;
-
-        } while (readRes && ftell(fp) >= 2 && readLineCnt < atoi(line_arg));
-
-        readByte = 0;
-
-        fseek(fp, -1, SEEK_CUR);
-
-        while (fread(&readByte, 1, 1, fp) && readLineCnt >= 0)
-        {
-            printf("%c", readByte);
-            if (readByte == '\n')
-                readLineCnt--;
-        }
-    }
 
     if (help_flag)
     {
@@ -209,12 +125,119 @@ Mandatory arguments to long options are mandatory for short options too.\n\
   -z, --zero-terminated    line delimiter is NUL, not newline\n\
       --help     display this help and exit\n\
       --version  output version information and exit\n");
+
+    return 0;
+
     }
 
     if (version_flag)
     {
         printf("tail version 1.0 \nWritten by S.Can İNEL, on July 2022\nDedicated to my sweetheart Ms.Şevval :)\n");
+
+        return 0;
     }
 
+    if(bytes_flag && lines_flag)
+    {
+        printf("byte and line options cannot be used together\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp = NULL;
+
+    if (inputFileName == NULL || strcmp(inputFileName, "-") == 0)
+    {
+        fp = stdin;
+    }
+    else
+    {
+        fp = fopen(inputFileName, "r");
+        if (fp == NULL)
+        {
+            perror("fopen");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fseek(fp, 0, SEEK_END); // dosya göstericisini dosyanın sonuna konumlandırır
+
+    if (bytes_flag)
+    {
+        int readRes = 0;
+        char readByte = 0;
+        int readCount = 0;
+
+        do
+        {
+            fseek(fp, -2, SEEK_CUR);
+            readRes = fread(&readByte, 1, 1, fp);
+            readCount++;
+
+        } while (readRes && ftell(fp) >= 2 && readCount < atoi(byte_arg));
+
+        readByte = 0;
+
+        fseek(fp, -1, SEEK_CUR);
+
+        while (fread(&readByte, 1, 1, fp))
+        {
+            printf("%c", readByte);
+        }
+    }
+
+    if (lines_flag)
+    {
+
+        int readRes = 0;
+        char readByte = 0;
+        int readLineCnt = 0;
+
+        do
+        {
+            fseek(fp, -2, SEEK_CUR);
+            readRes = fread(&readByte, 1, 1, fp);
+            if (readByte == '\n')
+                readLineCnt++;
+
+        } while (readRes && ftell(fp) >= 2 && readLineCnt < atoi(line_arg));
+
+        readByte = 0;
+
+        fseek(fp, -1, SEEK_CUR);
+
+        while (fread(&readByte, 1, 1, fp) && readLineCnt >= 0)
+        {
+            printf("%c", readByte);
+            if (readByte == '\n')
+                readLineCnt--;
+        }
+    }
+
+    if (!bytes_flag && !lines_flag)
+    {
+        int readRes = 0;
+        char readByte = 0;
+        int readLineCnt = 0;
+
+        do
+        {
+            fseek(fp, -2, SEEK_CUR);
+            readRes = fread(&readByte, 1, 1, fp);
+            if (readByte == '\n')
+                readLineCnt++;
+
+        } while (readRes && ftell(fp) >= 2 && readLineCnt < DEFAULT_LINE_NUMBER);
+
+        readByte = 0;
+
+        fseek(fp, -1, SEEK_CUR);
+
+        while (fread(&readByte, 1, 1, fp) && readLineCnt >= 0)
+        {
+            printf("%c", readByte);
+            if (readByte == '\n')
+                readLineCnt--;
+        }
+    }
     return 0;
 }
